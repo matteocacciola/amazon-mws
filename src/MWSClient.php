48f6157d51cc41eec54e0879bea7e6926a414523
Matteo Cacciola <matteo.cacciola@gmail.com>
2019-04-11 00:32:43 +0200

    + added marketplaces from EAU + changed the PostProduct (txt file must be sent, to CSV https://sellercentral.amazon.com/forums/t/-post-flat-file-listings-data--feed-type-not-working-in-mws/36269)

--- src/MWSClient.php
+++ src/MWSClient.php
@@ -30,19 +30,22 @@ class MWSClient
     ];
 
     private $MarketplaceIds = [
+        'A2Q3Y263D00KWC' => 'mws.amazonservices.com',
         'A2EUQ1WTGCTBG2' => 'mws.amazonservices.ca',
-        'ATVPDKIKX0DER'  => 'mws.amazonservices.com',
         'A1AM78C64UM0Y8' => 'mws.amazonservices.com.mx',
+        'ATVPDKIKX0DER'  => 'mws.amazonservices.com',
+        'A2VIGQ35RCS4UG' => 'mws.amazonservices.ae',
         'A1PA6795UKMFR9' => 'mws-eu.amazonservices.com',
         'A1RKKUPIHCS9HS' => 'mws-eu.amazonservices.com',
         'A13V1IB3VIYZZH' => 'mws-eu.amazonservices.com',
+        'A1F83G8C2ARO7P' => 'mws-eu.amazonservices.com',
         'A21TJRUUN4KGV'  => 'mws.amazonservices.in',
         'APJ6JRA9NG5V4'  => 'mws-eu.amazonservices.com',
-        'A1F83G8C2ARO7P' => 'mws-eu.amazonservices.com',
+        'A33AVAJ2PDY3EV' => 'mws-eu.amazonservices.com',
+        'A39IBJ37TRP1C6' => 'mws.amazonservices.com.au',
         'A1VC38T7YXB528' => 'mws.amazonservices.jp',
         'AAHKV2X7AFYLW'  => 'mws.amazonservices.com.cn',
-        'A39IBJ37TRP1C6' => 'mws.amazonservices.com.au',
-        'A2Q3Y263D00KWC' => 'mws.amazonservices.com'
+
     ];
 
     protected $debugNextFeed = false;
@@ -1268,8 +1271,7 @@ class MWSClient
             );
         }
 
-        return $this->SubmitFeed('_POST_FLAT_FILE_LISTINGS_DATA_', $csv);
-
+        return $this->SubmitFeed('_POST_FLAT_FILE_LISTINGS_DATA_', $csv->__toString());
     }
 
     /**
